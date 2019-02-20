54a5e7fd87ab27a5371bd62f1b54a024a526cb05
Kamil Tomaszewski <kamil.tomaszewski@sony.com>
2019-02-20 13:50:15 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
+
+io.on('connection', function(socket){
+  io.emit('position', '50.00, 55.00');
+  console.log('emit');
+});
