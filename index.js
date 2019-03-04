675e17ce6a093d7731c086bedb2dff8657cc6b18
Kamil Tomaszewski <kamil.tomaszewski@sony.com>
2019-03-04 14:41:51 +0100

    Add Leaflet map

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var mqtt = require('mqtt')
+var moment = require('moment');
+var client  = mqtt.connect('<username>:<password>@eu.thethings.network:1883')
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
@@ -11,6 +14,24 @@ http.listen(3000, function(){
 });
 
 io.on('connection', function(socket){
-  io.emit('position', '50.00, 55.00');
   console.log('emit');
 });
+
+client.on('connect', function () {
+  client.subscribe('+/devices/+/up', function (err) {
+    if (err) {
+      console.log('subscribe error');
+    }
+  })
+})
+ 
+client.on('message', function (topic, message) {
+  var mess = JSON.parse(message.toString());
+  var position = {
+    id: mess.dev_id,
+    time: moment(mess.metadata.time).format('YYYY-MM-DD HH:mm:ss'),
+    lat: mess.payload_fields.lat,
+    lng: mess.payload_fields.lng,
+  }
+  io.emit('position', position);
+})
